5b4d28789fed838f9ed80398c449e79c7235efbe
Maxim Dobryakov <closer.main@gmail.com>
2013-02-08 22:01:05 +0400

    Add empty EventSelectorController

---
+++ memoirs/iPhone/Events/EventSelectorController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  EventSelectorController_iPhone.h
+//  memoirs
+//
+//  Created by Maxim Dobryakov on 2/8/13.
+//  Copyright (c) 2013 protonail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventSelectorController_iPhone : UIViewController
+
+@end
---
+++ memoirs/iPhone/Events/EventSelectorController_iPhone.m
@@ -0,0 +1,24 @@
+//
+//  EventSelectorController_iPhone.m
+//  memoirs
+//
+//  Created by Maxim Dobryakov on 2/8/13.
+//  Copyright (c) 2013 protonail.com. All rights reserved.
+//
+
+#import "EventSelectorController_iPhone.h"
+
+@interface EventSelectorController_iPhone ()
+
+@property (weak, nonatomic) IBOutlet UITableView *ctlTableView;
+
+@end
+
+@implementation EventSelectorController_iPhone
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
